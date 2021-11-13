45abe0d5d499d934002c28ccfb768dd81a675290
joaoaluz <engluzjoao@gmail.com>
2021-11-13 03:04:37 -0300

    Adicionado test de envio e teste de recebimento

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,53 @@
 #include <Arduino.h>
 #include "TimerOne.h" // Biblioteca usada para o timer
 
-#define rs485Comunication 3 // Define porta de que define o estado da comunicação rs485
 #define packageTimer 8020 // Define o timer para 8,020 milisegundos o dobro do tempo de tramissão 
+#define rs485Comunication 3 // Define porta de que define o estado da comunicação rs485
+
+bool test = true;
+byte msgToSend[5];
 
 /**
  * Funcão que envia o pacote ao cfp
 **/
-void sendMsg(byte msg[]) {
+void sendMsg() {
   Serial.println("Start send msg.");
-
-  for (byte i = 0; i < sizeof(msg); i++)
+  digitalWrite(rs485Comunication, HIGH);
+  
+  for (byte i = 0; i < sizeof(msgToSend); i++)
   {
-    Serial.write(msg[i]); // Envio de cada bit da mensagem
-    Serial.println(msg[i], HEX);
+    Serial1.write(msgToSend[i]); // Envio de cada bit da mensagem
+    Serial.println(msgToSend[i], HEX);
   }
+
+  Serial1.flush();
 };
 /** ------------------------------ **/
 
 /**
  * Função que recebe o endereço do cfp e um id de msg e manda a msg ao cpf
 **/
-byte* makeMsg(int cfpAddress, int msgId) {
-  byte packageMsg[5];
+void makeMsg(int cfpAddress, int msgId) {
+  msgToSend[0] = 0x80; // byte inicial, padrão no inicio do pacote;
+  msgToSend[1] = 0x03; // byte de endereço do cfp
+  msgToSend[2] = 0x01; // tamanho, variavel padrao para as msgs que vamos mandar ao cfp
+  msgToSend[4] = 0x0D;
 
-  packageMsg[0] = 0x80; // byte inicial, padrão no inicio do pacote;
-  packageMsg[1] = 0x03; // byte de endereço do cfp
-  packageMsg[2] = 0x01; // tamanho, variavel padrao para as msgs que vamos mandar ao cfp
-
-  /**  **/
+  /** Switch que define codigo de operacao cfp **/
   switch (msgId)
   {
   case 1: // id: 1 - Ler Modo de funcionamento
-     packageMsg[3] = 0x01;
+     msgToSend[3] = 0x01;
     break;
   default:
     break;
   }
 
-  return packageMsg;
+  for (byte i = 0; i < sizeof(msgToSend); i++)
+  {
+    Serial.println(msgToSend[i], HEX);
+  }
+  
 }
 /** ------------------------------ **/
 
@@ -56,31 +65,6 @@ void timerCallback()
 /** ------------------------------ **/
 
 /**
- * Arduino setup 
- * **/
-
-void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600); 
-
-  pinMode(rs485Comunication, OUTPUT); // Define a porta que manipula a recebimento e envio de msgs RS485
-  digitalWrite(rs485Comunication, LOW); // Espera receber mensagem
-
-  Timer1.attachInterrupt(timerCallback);  // Define a função que irá ser executada a cada fim de espera do pacote
-}
-/** ------------------------------ **/
-
-/**
- * Arduino Loop 
-**/
-void loop() {
-  // digitalWrite(rs485Comunication, HIGH);
-  // sendMsg(); // Envia msg
-  // digitalWrite(rs485Comunication, LOW); // Espera receber mensagem
-}
-/** ------------------------------ **/
-
-/**
  * interrupção serial nas portas TX1 = 18, RX1 = 19
 **/
 void serialEvent1() {
@@ -91,5 +75,35 @@ void serialEvent1() {
   Serial.println(incomingByte, HEX);
   
   Timer1.initialize(packageTimer); // Inicia o timer que aguardar o pacote inteiro
+}
+/** ------------------------------ **/
+
+/**
+ * Arduino setup 
+ * **/
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600); 
+
+  pinMode(rs485Comunication, OUTPUT); // Define a porta que manipula a recebimento e envio de msgs RS485
+ 
+  Timer1.attachInterrupt(timerCallback);  // Define a função que irá ser executada a cada fim de espera do pacote
+}
+/** ------------------------------ **/
+
+/**
+ * Arduino Loop 
+**/
+void loop() {
+  if (test)
+  {
+    digitalWrite(rs485Comunication, HIGH);                                                         
+    makeMsg(1, 1);
+    sendMsg();
+  }
+  
+  test = false;
+  digitalWrite(rs485Comunication, LOW); // Espera receber mensagem
 }
 /** ------------------------------ **/
